b1daf6166d554e0742ea19c58618c5d12bde5c47
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-04-26 12:31:16 +1000

    Feat: publish File and SiteTree relations when the Link is 'published'

--- src/Extensions/LinkExtension.php
+++ src/Extensions/LinkExtension.php
@@ -11,6 +11,16 @@ use SilverStripe\ORM\DataExtension;
  */
 class LinkExtension extends DataExtension {
 
+    /**
+     * @var array
+     * When the Link record is 'published', publish these relationships
+     * via the ownership API
+     */
+    private static $owns = [
+        'File',
+        'SiteTree'
+    ];
+
     public function TitleWithURL() {
         $title = $this->owner->Title;
         $url = $this->owner->getLinkURL();
